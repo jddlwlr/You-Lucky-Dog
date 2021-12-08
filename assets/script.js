90cd76c47798604488939154fdfdcaff62b6d0f3
Narcissa <91091964+mel-ificent@users.noreply.github.com>
2021-12-07 21:33:58 -0500

    added Petfinder API and initial form submit handler

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
-var requestUrl = 'https://api.thedogapi.com/v1/breeds?breed_id=air&api_key=4967806f-5944-4473-9348-b6101abfe209';
+//Event Listener variables
+var searchFormEl = document.querySelector('#dog-form');
+var zipcodeInputEl = document.querySelector('#zipcode');
 
-fetch(requestUrl)
+//var requestUrl = 'https://api.thedogapi.com/v1/breeds?breed_id=air&api_key=4967806f-5944-4473-9348-b6101abfe209';
+
+//fetch(requestUrl)
 //   .then(response => response.JSON())
-  .then(data => console.log(data)); 
+ // .then(data => console.log(data)); 
     
   
 //   console.log(response);
@@ -13,3 +17,69 @@ fetch(requestUrl)
 //       console.log(data[i].user.login);
 //     }
 //   });
+
+
+//Submit Form event handler
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var zipcode = zipcodeInputEl.value.trim();
+
+  if (zipcode) {
+      getToken(zipcode);
+      zipcodeInputEl.value = '';
+
+    
+  } else {
+
+    alert('Please enter a zip code');
+  }
+};
+
+//Create initial authorization call to PetFinder
+
+var getToken = function (zipcode) {
+  var apiTokenUrl = 'https://api.petfinder.com/v2/oauth2/token?grant_type=client_credentials&client_id=GxKYrnyVF48Z4i8rOwDRcyoCtA3hpU1fsY7rFg2nUikNFY46eX&client_secret=9NJHpYc4dOdTMoUhrovPZ8b67crTmVopJOyJuBXY';
+  fetch(apiTokenUrl, {
+    method:'POST',
+  })
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          getPetResults(data, zipcode);
+        });
+      } else {
+        alert('Unable to connect to Petfinder');
+      }
+    })
+
+};
+
+var getPetResults = function (results, zipcode){
+
+var petFinderURL ='https://api.petfinder.com/v2/animals?type=dog&status=adoptable&location='+ zipcode;
+var bearer = 'Bearer ' + results.access_token;
+
+fetch(petFinderURL,{ 
+  headers: {
+     'Authorization': bearer,
+  }
+})
+
+.then(function (response) {
+  if (response.ok) {
+    response.json().then(function (data) {
+      showPetResults(data);
+    });
+  } else {
+    alert('Unable to connect to Petfinder');
+  }
+})
+};
+
+var showPetResults = function (results){
+  console.log(results);
+
+};
+
+searchFormEl.addEventListener('submit', formSubmitHandler);
