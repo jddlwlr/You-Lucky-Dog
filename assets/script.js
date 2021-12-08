c5d24852b43ea4eb00e8d649bf201c6a7b8fe30e
Jesse Lawler <jedd.lawler@gmail.com>
2021-12-07 19:34:59 -0700

    Adding DOG API functionality, minor tweaks in HTML to allow dynamic page updayes

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,28 @@
-var requestUrl = 'https://api.thedogapi.com/v1/breeds?breed_id=air&api_key=4967806f-5944-4473-9348-b6101abfe209';
+var dogUrl = 'https://api.thedogapi.com/v1/breeds/search?q=lab&api_key=4967806f-5944-4473-9348-b6101abfe209';
+var breedData = document.getElementById('breed-data');
 
-fetch(requestUrl)
-//   .then(response => response.JSON())
-  .then(data => console.log(data)); 
+// headers['x-api-key'] = "4967806f-5944-4473-9348-b6101abfe209"
+
+fetch(dogUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data)
+  
+  var breedWeight = document.createElement('ul')
+  var breedTemperment = document
+
+  breedWeight.innerHTML = '<li>' + 'Weight: ' + data[0].weight.imperial + 'lbs'+'</li>';
+ 
+breedData.append(breedWeight);
+
+document.getElementById('dog-breed').textContent = data[0].name + '\'s';
+  
+});
+// fetch(requestUrl)
+// //   .then(response => response.JSON())
+//   .then(console.log()); 
     
   
 //   console.log(response);
